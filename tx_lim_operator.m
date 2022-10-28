98063e6a3b876f5fa27d9f7053c3603f378ee570
Tongtong Xu <txu@Tongtongs-MacBook-Pro.local>
2022-10-28 11:23:14 -0600

    Added solver of LIM operator

---
+++ tx_lim_operator.m
@@ -0,0 +1,20 @@
+function [L,Q] = tx_lim_operator(X0,Xtau,tau0)
+%-----------------
+% Input: X(t), X(t+tau0), and the training lag tau0
+% Output: L - LIM operator; Q - noise covariance
+% 
+% T. Xu
+% 2021
+%-----------------
+
+C0 = X0*X0'/(size(X0,2)-1);
+Ctau = Xtau*X0'/(size(X0,2)-1);
+
+% linear operator
+G = Ctau/C0;
+L = logm(G)/tau0;
+
+% noise statistics
+Q = -1*(L*C0+C0*L');
+
+end
